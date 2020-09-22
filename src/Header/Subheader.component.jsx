bcf092e6cb9eaf4e1144b40fdccdafc84182039f
JoaquinManchenoP <manchenojj@g.cofc.edu>
2020-09-22 15:01:53 -0400

    Subheader compleated
    Compleating the styling for the subheader

--- src/Header/Subheader.component.jsx
+++ src/Header/Subheader.component.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Subheader.css";
+import PersonPinCircleIcon from "@material-ui/icons/PersonPinCircle";
 
 export default function Subheader() {
   return (
     <div className="subheader">
-      <div className="location">
-        {/*location icon */}
-        <span className="location__line1">Hello</span>
-        <span className="location_line2">Select your adress</span>
+      <div className="location__withIcon">
+        <div className="just__location__icon">
+          <PersonPinCircleIcon />
+        </div>
+        <div className="location">
+          <span className="location__line1">Hello</span>
+          <span className="location_line2">Select your adress</span>
+        </div>
       </div>
 
       <div className="subheader__menu">
@@ -16,10 +21,12 @@ export default function Subheader() {
         <span className="subheader__option">Amazon Basics</span>
         <span className="subheader__option">New Releases</span>
         <span className="subheader__option">Today's Deals</span>
+        <span className="subheader__option">Whole Foods</span>
+        <span className="subheader__option">Gift Cards</span>
       </div>
 
-      <div className="podcast__option">
-        <span>Stram podcasts FREE</span>
+      <div className="supplies__option">
+        <span>Low prices on school supplies</span>
       </div>
     </div>
   );
